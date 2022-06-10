6c14c6fa2ca6bb33f5e83f293c74fbcf4406c5eb
ross-grinvalds <ross.grinvalds@lilly.com>
2022-06-10 08:15:45 -0400

    update /Users/l284942 reference

--- initialize.sh
+++ initialize.sh
@@ -1,14 +1,11 @@
 if [[ $OSTYPE = "darwin"* || $OSTYPE = "linux-gnu" ]]; then
-	if [ ! -d "$HOME/.dotfiles" ]; then
-		mkdir $HOME/.dotfiles
-	fi
 
-  export DOTFILES="$HOME/.dotfiles"
+	export DOTFILES="$HOME"
 
 	if [ ! -d "$DOTFILES/nvim" ]; then
 		mkdir "$DOTFILES/nvim"
 	fi
-
+	
 	rsync -avr --exclude=".doc*" --exclude=".git*" --exclude="initialize.sh" --exclude="README.md" ./ "$DOTFILES/nvim"
 	
 	valid_responses=('y' 'n')
